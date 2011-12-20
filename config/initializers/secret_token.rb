9dd9f517f5ce9b02a51e1e4e818914f636bd31da
khy6 <khy.trading@gmail.com>
2011-12-20 17:37:56 -0500

    Improved the README file

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sgg::Application.config.secret_token = 'c02e0c2a1f7bbfd45a04380b0fc7f69cce6ca7ecb013bb0ee530de08abd26c220864718163053b003bb69ec62016aa2fc3b60d296120c14de47821b9885c1695'
